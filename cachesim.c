c667fad8cb66c925c6c45dc791b58ca9c0b0a38b
chanwooDev <pove2019@gmail.com>
2021-12-06 04:04:05 +0900

    [feat]  block, set, cache구조체 생성

--- cachesim.c
+++ cachesim.c
@@ -1,61 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-
 void getParameter(int argc, char* argv[]);
-void getRequest();
+void setRequestList();
+void init_cache();
+
 
 typedef	struct Request {
-	char* address;
+	long long address;
 	char rw;
-	char* data;
-}Request;
+	long long data;
+}RequestStruct;
+typedef RequestStruct* Request;
 
-typedef	struct Cache {
-	char* tag;
-	int** data;
+typedef struct Block {
+	int tag;
+	int data;
 	bool dirty;
 	bool valid;
-}Cache;
+	int rank;
+}BlockStruct;
+typedef BlockStruct* Block;
+
+typedef struct Set {
+	Block* block_pp;
+}SetStruct;
+typedef SetStruct* Set;
+
+typedef	struct Cache {
+	Set* set_pp;
+}CacheStruct;
+typedef CacheStruct* Cache;
 
 typedef	struct Memory {
 	int adress;
 	int data;
-}Memory;
-
-//구조체 선언
-//reqeust
-//cache
-//memory
-//set 구조체???
-
+}MemoryStruct;
+typedef MemoryStruct* Memory;
 Memory* memoryList;
 Cache* cacheList;
-Request* RequestList;
+Request* requestList;
 int cache_size = 0;
 int set_size = 0;
 int block_size = 0;
 char* fileName;
-
+int requestListCount = 0;
 int main(int argc, char* argv[]) {
 
 	getParameter(argc, argv);
+	setRequestList();
+	init_cache();
+	//for (int i = 0; i < requestListCount; i++) {
+		//printf("addr : %lld R/W : %c, Data: %lld\n", requestList[i]->address, requestList[i]->rw, requestList[i]->data);
 
-	FILE* fp = 0;
-	
-	fp = fopen(fileName, "r");
-	if (fp == NULL)
-	{
-		printf("can't open");
-		fflush(stdout);
-		exit(-1);
+	//}
+	for (int i = 0; i < requestListCount; i++) {
+		requestList[i];//hit인지 아닌지
+
 	}
 	
-	
 
-	getRequest()
 
 	//init request -> mm memory 동적할당
 
@@ -103,20 +109,75 @@ void getParameter(int argc, char* argv[]) {
 		}
 		if (strncmp("-f", argv[i], 2) == 0) {
 			int t_length = strlen(argv[i]);
-			char* context = NULL;
-			char* ptr = strtok_s(argv[i], "=", &context);      // " " 공백 문자를 기준으로 문자열을 자름, 포인터 반환
-			fileName = strtok_s(NULL, "=", &context);
+			char* ptr = strtok(argv[i], "=");      // " " 공백 문자를 기준으로 문자열을 자름, 포인터 반환
+			fileName = strtok(NULL, "=");
 			printf("%s", fileName);
 		}
 	}
 }
 
 void setRequestList() {
+	FILE* trace = 0;
+	char temp[101];
+	trace = fopen(fileName, "r");
+	if (trace == NULL)
+	{
+		printf("can't open");
+		fflush(stdout);
+		exit(-1);
+	}
+
+	while (!feof(trace)) {
+		fgets(temp, 101, trace);
+		if (strcmp(temp, "\n") == 0) {
+			break;
+		}
+		requestListCount++;
+	}
+	fclose(trace);
+
+	requestList = malloc(sizeof(Request) * requestListCount);
+	
+	int top = -1;
+	trace = fopen(fileName, "r");
+	if (trace == NULL)
+	{
+		printf("can't open");
+		fflush(stdout);
+		exit(-1);
+	}
+	while (!feof(trace)) {
+		fgets(temp, 101, trace);
+		if (strcmp(temp, "\n") == 0) {
+			break;
+		}
+		temp[strlen(temp) - 1] = '\0';
+		char* ptr = strtok(temp, " ");
+		requestList[++top] = malloc(sizeof(RequestStruct));
+		requestList[top]->address = strtoll(ptr, NULL, 16);
+		ptr = strtok(NULL, " ");
+		requestList[top]->rw = ptr[0];
+		if (ptr[0] == 'W') {
+			ptr = strtok(NULL, " ");
+			requestList[top]->data = atoi(ptr);
+		}
+	}
+	fclose(trace);
+}
+
+void init_cache() {
+	
+
 
 
 }
 
-void getRequest() {
+bool Hit() {
 
 
+	//R
+
+	//W
+
+	return false;
 }
